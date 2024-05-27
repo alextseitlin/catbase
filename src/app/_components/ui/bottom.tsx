2712d187a868c14d9870af1592b4ec8c766ec961
Alex Tseitlin <alextseitlin@Alexs-MBP.lan>
2024-05-27 13:02:26 +0300

    submit support

--- src/app/_components/ui/bottom.tsx
+++ src/app/_components/ui/bottom.tsx
@@ -1,16 +1,31 @@
-import Link from "./link";
+import Link from "./Link";
 
 type Props = {
   children?: React.ReactNode;
+  onClick?: Function;
   href?: string;
+  type?: string;
 };
-function Bottom({ children, href }: Props) {
+const baseClassName = "bg-emerald-400 py-2 px-3 inline-block cursor-pointer";
+function Bottom({ children, href, onClick, type }: Props) {
   if (href) {
-    return <Link href={href}>children</Link>;
+    return (
+      <Link href={href} className={`${baseClassName}`} onClick={onClick}>
+        children
+      </Link>
+    );
+  }
+
+  if (type === "submit") {
+    return (
+      <input className={`${baseClassName}`} value={children} type="submit" />
+    );
   }
 
   return (
-    <div className="bg-emerald-400 py-2 px-3 inline-block">{children}</div>
+    <div className={`${baseClassName}`} onClick={onClick} type={type}>
+      {children}
+    </div>
   );
 }
 export default Bottom;
