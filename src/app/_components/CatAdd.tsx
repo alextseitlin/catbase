1504bb7dd3665a5695329483054c2dd915e03a87
Alex Tseitlin <alextseitlin@Alexs-MBP.lan>
2024-05-28 07:20:37 +0300

    new paths

--- src/app/_components/CatAdd.tsx
+++ src/app/_components/CatAdd.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 
 import { useState } from "react";
-import Button from "@/components/ui/Button";
+import Button from "@/ui/Button";
 
 type Props = {
   setCatsList: Function;
